7de54689532d4a3d7326abb0a937a43ebc58f95f
masaya <hope.is.a.waking.dream.8.1@gmail.com>
2021-01-22 10:06:16 +0900

    modify js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 {
   function setWord() {
-    word = words[Math.floor(Math.random() * words.length)];
+    word = words.splice(Math.floor(Math.random() * words.length), 1)[0];
     target.textContent = word;
     loc = 0;
   }
@@ -10,9 +10,19 @@
   const words = ["red", "blue", "pink", "black"];
   let word;
   let loc = 0;
+  let startTime;
+  let isPlaying = false;
   const target = document.getElementById("target");
 
-  setWord();
+  document.addEventListener("click", () => {
+    if (isPlaying === true) {
+      return;
+    }
+
+    isPlaying = true;
+    startTime = Date.now();
+    setWord();
+  });
 
   document.addEventListener("keydown", (e) => {
     if (e.key !== word[loc]) {
@@ -25,6 +35,12 @@
     target.textContent = "_".repeat(loc) + word.substring(loc);
 
     if (loc === word.length) {
+      if (words.length === 0) {
+        const elapsedTime = ((Date.now() - startTime) / 1000).toFixed(2);
+        const result = document.getElementById("result");
+        result.textContent = `Finished!  ${elapsedTime} seconds!`;
+        return;
+      }
       setWord();
     }
   });
